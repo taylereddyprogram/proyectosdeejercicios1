ac9b7124ddf24f9620f319d406f4801e10c5c5ce
taylereddyprogram <taylereddymanuel@gmail.com>
2016-12-09 01:11:09 +0100

    Add files via upload

---
+++ rectangulo.java
@@ -0,0 +1,16 @@
+
+package arearectangulo;
+
+
+public class rectangulo {
+
+   
+    public static void main(String[] args) {
+        
+        int base=10;
+        int altura=5;
+        
+        int area =base*altura;
+    }
+    
+}
